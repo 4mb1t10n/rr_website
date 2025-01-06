834997fdbf4525c8fbe59f806552bb12a4efa5a8
Misha Omelyanchum <omelmisha@gmail.com>
2025-01-06 23:42:45 +0200

    upd

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM wordpress:php8.1-apache
 
 COPY ./wp-content /var/www/html/wp-content
 
+COPY ./wp-config.php /var/www/html/wp-config.php
+
 RUN echo "ServerName localhost" > /etc/apache2/conf-available/servername.conf && \
     a2enconf servername
 
--- wp-config.php
+++ wp-config.php
@@ -1,26 +1,24 @@
 <?php
-define('DB_NAME', getenv('DB_NAME'));
-define('DB_USER', getenv('DB_USER'));
-define('DB_PASSWORD', getenv('DB_PASSWORD'));
-define('DB_HOST', getenv('DB_HOST'));
+define('DB_NAME', getenv('MYSQLDATABASE'));
+define('DB_USER', getenv('MYSQLUSER'));
+define('DB_PASSWORD', getenv('MYSQLPASSWORD'));
+define('DB_HOST', getenv('MYSQLHOST') . ':' . getenv('MYSQLPORT'));
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
-define('WP_HOME', getenv('WP_HOME'));
-define('WP_SITEURL', getenv('WP_SITEURL'));
+define('AUTH_KEY',         'gjQ2%%[*C-T.BU>F:-VPmT+6Jx:~w05oY(yf)- 93pn+]}2#X[:*Ao1>~<3NJz<v');
+define('SECURE_AUTH_KEY',  'lfJX(woAocmRf7mN@`B;F$U<rj{vbx/f)G~A{F[nZAM[g?cx@Iu1vd~QFmO3-:9i');
+define('LOGGED_IN_KEY',    'dE|JP72 spto:PK=}SB$Zss{ayC)~4VlTJ|oV(]~~$i77|vnJJo,|A?J NthB,@`');
+define('NONCE_KEY',        'O|1tGp<=y.:C(r1=Q5Wf07h6ahi:yDS4t&-H#8;a*vJ`6Q9BeKECB7<8z9~gLz7T');
+define('AUTH_SALT',        'KD&33R6^ySTN{4^s^Dw|+Rc(iI.-$;~ZGVOj?M2=t]u:t_xb15ws;kjRLHH7pvHZ');
+define('SECURE_AUTH_SALT', 'gi=?_|K*|.%OW|7n|[>Cv{E(|M|rULb.j^]68$RZ*V+z^9lD1<3M mTjU&c8-=~w');
+define('LOGGED_IN_SALT',   ',MzTHe&C(^Gl%MN4R=);_H;OBloL`),r8[DY#;-S8V}v^+X) QfZD]Hfz[ZMj2wP');
+define('NONCE_SALT',       'e!~:[|J+`42srHBWO<)c%S8~*P]bS>xBd1Z?G#%)0u,M-=qav?.t/|nFJB&^AB3>');
 
-define('AUTH_KEY', 'your_unique_key');
-define('SECURE_AUTH_KEY', 'your_unique_key');
-define('LOGGED_IN_KEY', 'your_unique_key');
-define('NONCE_KEY', 'your_unique_key');
-define('AUTH_SALT', 'your_unique_key');
-define('SECURE_AUTH_SALT', 'your_unique_key');
-define('LOGGED_IN_SALT', 'your_unique_key');
-define('NONCE_SALT', 'your_unique_key');
-
-define('UPLOADS', 'wp-content/uploads');
+$table_prefix = 'wp_';
 
 define('WP_DEBUG', false);
-
-define('FORCE_SSL_ADMIN', true);
-define('FORCE_SSL_LOGIN', true);
+if (!defined('ABSPATH')) {
+    define('ABSPATH', __DIR__ . '/');
+}
+require_once ABSPATH . 'wp-settings.php';
